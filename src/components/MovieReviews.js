b3a11306fefbaad80922e4c2da9d79dfb31ab014
AfraaAlobaid <afraa.obaid1@gmail.com>
2021-07-04 20:16:23 +0930

    implement MoviewReviews component

--- src/components/MovieReviews.js
+++ src/components/MovieReviews.js
@@ -1,1 +1,11 @@
-// Code MovieReviews Here
+import React, { Component } from "react";
+
+const MovieReviews = ({ reviews }) => (
+  <div className="review-list">
+    {reviews.map((review) => (
+      <div className="review">{review}</div>
+    ))}
+  </div>
+);
+
+export default MovieReviews;
