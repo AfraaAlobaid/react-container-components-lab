50decfc61f2a00c4c5bc5a840d036b650be4e1c9
AfraaAlobaid <afraa.obaid1@gmail.com>
2021-07-04 21:01:08 +0930

    implement SearchableMoviewReviewsContainer component

--- src/components/MovieReviews.js
+++ src/components/MovieReviews.js
@@ -1,11 +1,19 @@
 import React, { Component } from "react";
 
-const MovieReviews = ({ reviews }) => (
+const MovieReviews = ({ reviews }) => {
+    if(reviews)
+        return (
   <div className="review-list">
     {reviews.map((review) => (
       <div className="review">{review.summary_short}</div>
     ))}
   </div>
 );
+return (
+    <div className="review-list">
+        No results found!
+    </div>
+)
+    }
 
 export default MovieReviews;
