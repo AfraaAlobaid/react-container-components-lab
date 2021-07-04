ec10e0d6a6e4136dbb3bdd846a4cc3b4307e1a38
AfraaAlobaid <afraa.obaid1@gmail.com>
2021-07-04 20:38:15 +0930

    implement LatestMoviewReviewsContainer component

--- src/components/LatestMovieReviewsContainer.js
+++ src/components/LatestMovieReviewsContainer.js
@@ -2,8 +2,34 @@ import React, { Component } from 'react';
 import 'isomorphic-fetch';
 import MovieReviews from './MovieReviews'
 
-const NYT_API_KEY = 'your-key-here';
+const NYT_API_KEY = 'BXhnWZGW17Ck2rGJ80WMzE3FuJVLPkwg';
 const URL = 'https://api.nytimes.com/svc/movies/v2/reviews/all.json?'
             + `api-key=${NYT_API_KEY}`;
 
-// Code LatestMovieReviewsContainer Here
+export default class LatestMovieReviewsContainer extends Component{
+    constructor(props){
+        super(props);
+
+        this.state = {
+            reviews: []
+        }
+    }
+
+    componentDidMount(){
+        fetch(URL)
+        .then((res) => res.json())
+        .then((data) => {
+            this.setState({
+                reviews: data.results
+            })
+        })
+    }
+
+    render(){
+        return (
+            <div className="latest-movie-reviews">
+                <MovieReviews reviews={this.state.reviews}/>
+            </div>
+        )
+    }
+}
--- src/components/MovieReviews.js
+++ src/components/MovieReviews.js
@@ -3,7 +3,7 @@ import React, { Component } from "react";
 const MovieReviews = ({ reviews }) => (
   <div className="review-list">
     {reviews.map((review) => (
-      <div className="review">{review}</div>
+      <div className="review">{review.summary_short}</div>
     ))}
   </div>
 );
